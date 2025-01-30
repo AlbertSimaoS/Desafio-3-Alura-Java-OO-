9b628ca9d5f57a304e94391ee45ade6767268f7d
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-30 14:48:43 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,21 @@
-public class Main {
+public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Produto produto1 = new Produto("Celular", 1500.00);
+
+        System.out.println("Nome do Produto: " + produto1.getNome());
+        System.out.println("Preço: " + produto1.getPreco());
+
+        produto1.aplicarDesconto(10);
+        System.out.println("Produto a vista: " + produto1.getPreco());
+
+        System.out.println("\n");
+
+        Produto produto2 = new Produto("TV", 2000.00);
+
+        System.out.println("Nome do Produto: " + produto2.getNome());
+        System.out.println("Preço: " + produto2.getPreco());
+
+        produto2.aplicarDesconto(15);
+        System.out.println("Preço a vista: " + produto2.getPreco());
     }
 }
