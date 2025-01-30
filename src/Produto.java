34f8d8b14ca2f1cdb3343e1dee1ff0fc52276831
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-30 14:49:01 -0300

    Update Produto.java

--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void aplicarDesconto(double percentual){
+        double desconto = preco * (percentual/100);
+        preco -= desconto;
+
+    }
 }
